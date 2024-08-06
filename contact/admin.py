415963e5fdb2c792c4549af470e7a039be81b866
alielsonfp <alielson_pequeno@hotmail.com>
2024-08-06 18:38:44 -0300

    ADD media root

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from contact import models
 # Register your models here.
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+  list_display = "id", "first_name", "last_name", "phone", "created_date"
+  ordering = "id",
+  # list_filter = "created_date",
+  search_fields = "id", "first_name", "last_name"
+  list_per_page = 50
+  list_max_show_all = 300
+  list_editable =  "last_name", "phone"
+  list_display_links = "id", "first_name",
