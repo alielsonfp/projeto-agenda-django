e70d3cf5b9d482b0817010fb9a70a331fd61fc32
alielsonfp <alielson_pequeno@hotmail.com>
2024-08-07 23:03:29 -0300

    Add search view

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,36 +1,68 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
 
 from contact.models import Contact
 
+
 def index(request):
-  contacts = Contact.objects.all().filter(show=True).order_by('-id')[:10]
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .order_by('-id')[10:20]
 
-  context ={
-    'contacts': contacts,
-    'site_tittle': 'Contatos - '
-  }
+    context = {
+        'contacts': contacts,
+        'site_title': 'Contatos - '
+    }
 
-  return render(
-    request,
-    'contact/index.html',
-    context
-  )
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - '
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
 
 def contact(request, contact_id):
-  #single_contact = Contact.objects.filter(pk=contact_id).first()
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
 
-  single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    context = {
+        'contact': single_contact,
+        'site_title': site_title
+    }
 
-  site_tittle = f'{single_contact.first_name} {single_contact.last_name} -'
-  
-  context ={
-    'contact': single_contact,
-    'site_tittle': site_tittle
-  }
-
-  return render(
-    request,
-    'contact/contact.html',
-    context
-  )
+    return render(
+        request,
+        'contact/contact.html',
+        context
+    )
